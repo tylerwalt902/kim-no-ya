bc4d8d38a07c92cafc4a501cef902b241891ad0b
tylerwalt902 <51471968+tylerwalt902@users.noreply.github.com>
2019-06-08 12:54:40 +0545

    Update index.jsx

--- src/components/PostTemplateDetails/index.jsx
+++ src/components/PostTemplateDetails/index.jsx
@@ -13,7 +13,7 @@ class PostTemplateDetails extends React.Component {
     const homeBlock = (
       <div>
         <Link className="post-single__home-button" to="/">
-          All Articles
+          All Episodes
         </Link>
       </div>
     )
@@ -64,13 +64,6 @@ class PostTemplateDetails extends React.Component {
             <hr />
             <p className="post-single__footer-text">
               {subtitle}
-              <a
-                href={`https://twitter.com/${author.twitter}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <br /> <strong>{author.name}</strong> on Twitter
-              </a>
             </p>
             {commentsBlock}
           </div>
